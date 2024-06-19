96d354f83fe7ad2256fb9f1f9a2e8b713e436588
GunKate <gokhfeld.ev@gmail.com>
2024-06-19 18:47:48 +0500

    ForeKrasota

--- jurnal1.py
+++ jurnal1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Создание данных
 data = {
     'name': ['Vasya', 'Vasya', 'Vasya', 'Vasya', 'Vasya',
              'Petya', 'Petya', 'Petya', 'Petya', 'Petya',
@@ -19,20 +20,29 @@ data = {
                  4, 3, 5, 4, 4]
 }
 
+# Создание DataFrame из данных
 df = pd.DataFrame(data)
-#print(df)
 
+# Вывод DataFrame для проверки (закомментировано)
+# print(df)
+
+# Расчет среднего значения оценок по каждому предмету
 mean_subject = df.groupby('subject')['estimate'].mean()
-print(mean_subject)
+print(f'среднее значение - {mean_subject}')
 
+# Расчет медианного значения оценок по каждому предмету
 median_subject = df.groupby('subject')['estimate'].median()
-print(median_subject)
+print(f'медианное значение - {median_subject}')
 
+# Расчет первого и третьего квартилей для оценок по математике
 Q1_math = df[df['subject'] == 'mat']['estimate'].quantile(0.25)
 Q3_math = df[df['subject'] == 'mat']['estimate'].quantile(0.75)
+
+# Расчет межквартильного размаха (IQR) для оценок по математике
 IQR = Q3_math - Q1_math
+print("межквартильного размаха (IQR): ", IQR)
 
+# Расчет стандартного отклонения оценок по каждому предмету
 std_subject = df.groupby('subject')['estimate'].std()
-
-print(IQR)
+print("стандартное отклонение по предмету: ")
 print(std_subject)
